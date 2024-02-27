93ee02e1db9785a29d85c28fcd5aa1a4d9005d1d
Maxi Alzugaray <maxialz99@hotmail.com>
2024-02-27 12:44:41 -0300

    creo archivo de barril para widgets de auth

---
+++ lib/feactures/auth/presentation/auth_presentation.dart
@@ -0,0 +1,11 @@
+export 'package:app_ciudadano_vc/feactures/auth/presentation/screens/auth_screen.dart';
+export 'package:app_ciudadano_vc/feactures/auth/presentation/screens/enter_code_scree.dart';
+export 'package:app_ciudadano_vc/feactures/auth/presentation/screens/register_screen.dart';
+export 'package:app_ciudadano_vc/feactures/auth/presentation/screens/welcome_screen.dart';
+export 'package:app_ciudadano_vc/feactures/home/presentation/screens/home_screen.dart';
+
+// ## WIDGETS ##
+export 'package:app_ciudadano_vc/feactures/auth/presentation/widgets/info_text.dart';
+export 'package:app_ciudadano_vc/feactures/auth/presentation/widgets/register_number_identification.dart';
+export 'package:app_ciudadano_vc/feactures/auth/presentation/widgets/register_personal_information.dart';
+export 'package:app_ciudadano_vc/feactures/auth/presentation/widgets/register_upload_files.dart';
