06f15fef3cf2f9dda0a97f6d6dca4ac05d498541
Maxi Alzugaray <maxialz99@hotmail.com>
2024-02-28 16:05:56 -0300

    agrego configuracion para libreria de camara

---
+++ lib/shared/auth_shared.dart
@@ -0,0 +1,1 @@
+export 'package:app_ciudadano_vc/shared/infraestructure/services/camera_gallery_service_impl.dart';
---
+++ lib/shared/infraestructure/services/camera_gallery_service_impl.dart
@@ -0,0 +1,26 @@
+import 'package:app_ciudadano_vc/shared/infraestructure/services/camera_gallery_service.dart';
+import 'package:image_picker/image_picker.dart';
+
+class CameraGalleryServiceImpl extends CameraGalleryService {
+  final ImagePicker _picker = ImagePicker();
+  @override
+  Future<String?> selectPhoto() async {
+    final XFile? photo = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (photo == null) return null;
+
+    print('Tenermos imagen =>>>>>>>> ${photo.path}');
+    return photo.path;
+  }
+
+  @override
+  Future<String?> takePhoto() async {
+    final XFile? photo = await _picker.pickImage(
+        source: ImageSource.camera, preferredCameraDevice: CameraDevice.rear);
+
+    if (photo == null) return null;
+
+    print('Tenermos imagen =>>>>>>>> ${photo.path}');
+    return photo.path;
+  }
+}
