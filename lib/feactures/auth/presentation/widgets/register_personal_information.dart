b27ea030e144ce207f2bcdfee4587ebbaf250685
Maxi Alzugaray <maxialz99@hotmail.com>
2024-02-28 16:05:42 -0300

    agrego steps para carga de datos en formulario de registro

--- lib/feactures/auth/presentation/widgets/register_personal_information.dart
+++ lib/feactures/auth/presentation/widgets/register_personal_information.dart
@@ -1,14 +1,69 @@
 import 'package:flutter/material.dart';
 
 class PersonalInformationForm extends StatelessWidget {
+  // final TextEditingController textController;
+  // final FocusNode focusNode;
   const PersonalInformationForm({
     super.key,
+    // required this.focusNode,
+    // required this.textController,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('_PersonalInformationForm'),
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            'Ingresa tus datos',
+            style: titleStyle,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                hintText: 'Nombre',
+                labelText: 'Nombre'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                hintText: 'Apellido',
+                labelText: 'Apellido'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                hintText: 'Correo electronico',
+                labelText: 'Correo electronico'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+                hintText: 'Fecha de nacimiento',
+                labelText: 'Fecha de nacimiento'),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/feactures/auth/presentation/widgets/register_upload_files.dart
+++ lib/feactures/auth/presentation/widgets/register_upload_files.dart
@@ -1,3 +1,5 @@
+import 'package:app_ciudadano_vc/shared/infraestructure/services/camera_gallery_service_impl.dart';
+import 'package:app_ciudadano_vc/shared/widgets/buttons/custom_filled_button.dart';
 import 'package:flutter/material.dart';
 
 class UploadFiles extends StatelessWidget {
@@ -7,8 +9,82 @@ class UploadFiles extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('_UploadFiles '),
+    final titleStyle = Theme.of(context).textTheme.titleLarge;
+    final subtitleStyle = Theme.of(context).textTheme.titleMedium;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            'Tóma algunas fotos',
+            textAlign: TextAlign.center,
+            style: titleStyle,
+          ),
+          Text(
+            '¡Ultimo paso!',
+            style: subtitleStyle,
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          CustomFilledButtom(
+            text: 'Foto frente DNI',
+            onPressed: () async {
+              final photoPaht = await CameraGalleryServiceImpl().takePhoto();
+              if (photoPaht == null) return;
+              photoPaht;
+            },
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          CustomFilledButtom(
+            text: 'Foto dorzo DNI',
+            onPressed: () async {
+              final photoPaht = await CameraGalleryServiceImpl().takePhoto();
+              if (photoPaht == null) return;
+              photoPaht;
+            },
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          CustomFilledButtom(
+            text: 'Foto de tu rostro',
+            onPressed: () async {
+              final photoPaht = await CameraGalleryServiceImpl().takePhoto();
+              if (photoPaht == null) return;
+              photoPaht;
+            },
+          )
+        ],
+      ),
     );
   }
 }
+
+
+
+// return Column(
+    //   children: [
+    //     TextButton.icon(
+    //       onPressed: () async {
+    //         final photoPah = await CameraGalleryServiceImpl().selectPhoto();
+    //         if (photoPah == null) return;
+    //         photoPah;
+    //       },
+    //       icon: Icon(Icons.browse_gallery),
+    //       label: Text('galeria'),
+    //     ),
+    //     TextButton.icon(
+    //       onPressed: () async {
+    //         final photoPah = await CameraGalleryServiceImpl().takePhoto();
+    //         if (photoPah == null) return;
+    //         photoPah;
+    //       },
+    //       icon: Icon(Icons.camera),
+    //       label: Text('camara'),
+    //     ),
+    //   ],
+    // );
