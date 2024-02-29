9fa5cda55971f1be4acc24e126bd030b77892607
Maxi Alzugaray <maxialz99@hotmail.com>
2024-02-29 13:39:58 -0300

    creo registerFormProvider

--- lib/feactures/auth/presentation/providers/register_form_provider.dart
+++ lib/feactures/auth/presentation/providers/register_form_provider.dart
@@ -1,10 +1,105 @@
+import 'package:app_ciudadano_vc/shared/infraestructure/inputs/inputs.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+final registerFormProvider =
+    StateNotifierProvider<RegisterFormNofier, RegisterFormState>((ref) {
+  return RegisterFormNofier();
+});
 
-// class RegisterFormStatus {
+class RegisterFormState {
+  final NameInput name;
+  final LastnameInput lastName;
+  final EmailInput email;
+  final DateOfBirth dateOfBirth;
+  final IdentificationNumber identificationNumber;
+  final GenderInput gender;
+  final FileInput profileImage;
+  final FileInput dniFrontImage;
+  final FileInput dniBackImage;
 
-// final String name;
-// final String lastName;
-// final String email;
+  final String? errorMessage;
+  final bool isLoading;
+  final bool isFormSubmitted;
 
+  RegisterFormState({
+    this.name = const NameInput.pure(),
+    this.lastName = const LastnameInput.pure(),
+    this.email = const EmailInput.pure(),
+    this.dateOfBirth = const DateOfBirth.pure(),
+    this.identificationNumber = const IdentificationNumber.pure(),
+    this.gender = const GenderInput.pure(),
+    this.profileImage = const FileInput.pure(),
+    this.dniFrontImage = const FileInput.pure(),
+    this.dniBackImage = const FileInput.pure(),
+    this.errorMessage,
+    this.isLoading = false,
+    this.isFormSubmitted = false,
+  });
 
-// }
+  RegisterFormState copyWith({
+    NameInput? name,
+    LastnameInput? lastName,
+    EmailInput? email,
+    DateOfBirth? dateOfBirth,
+    IdentificationNumber? identificationNumber,
+    GenderInput? gender,
+    FileInput? profileImage,
+    FileInput? dniFrontImage,
+    FileInput? dniBackImage,
+    String? errorMessage,
+    bool? isLoading,
+    bool? isFormSubmitted,
+  }) {
+    return RegisterFormState(
+      name: name ?? this.name,
+      lastName: lastName ?? this.lastName,
+      email: email ?? this.email,
+      dateOfBirth: dateOfBirth ?? this.dateOfBirth,
+      identificationNumber: identificationNumber ?? this.identificationNumber,
+      gender: gender ?? this.gender,
+      profileImage: profileImage ?? this.profileImage,
+      dniFrontImage: dniFrontImage ?? this.dniFrontImage,
+      dniBackImage: dniBackImage ?? this.dniBackImage,
+      errorMessage: errorMessage ?? this.errorMessage,
+      isLoading: isLoading ?? this.isLoading,
+      isFormSubmitted: isFormSubmitted ?? this.isFormSubmitted,
+    );
+  }
+}
+
+class RegisterFormNofier extends StateNotifier<RegisterFormState> {
+  RegisterFormNofier() : super(RegisterFormState());
+
+  onNameChange(String value) {
+    print('====onNameChange====>>>>>>>>>>>> $state');
+    state = state.copyWith(name: NameInput.dirty(value));
+  }
+
+  void onLastNameChange(String value) {
+    state = state.copyWith(lastName: LastnameInput.dirty(value));
+  }
+
+  void onEmailChange(String value) {
+    state = state.copyWith(email: EmailInput.dirty(value));
+  }
+
+  void onDateOfBirthChange(String value) {
+    state = state.copyWith(dateOfBirth: DateOfBirth.dirty(value));
+  }
+
+  onSubmittRegisterForm() {
+    -_validateForm();
+  }
+
+  _validateForm() {
+    if (state.name.value.isEmpty) {
+      state = state.copyWith(errorMessage: 'El nombre es obligatorio');
+      return;
+    }
+
+    // Otras validaciones para los demás campos del formulario...
+
+    // Si todas las validaciones pasan, establece el estado de éxito
+    state = state.copyWith(errorMessage: null, isFormSubmitted: true);
+  }
+}
