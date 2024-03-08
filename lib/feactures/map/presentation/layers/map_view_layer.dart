d6f5583a0cb0b502deb37a72ead28bf143f41a99
Maxi Alzugaray <maxialz99@hotmail.com>
2024-03-08 12:07:40 -0300

    pruebo marcadores

--- lib/feactures/map/presentation/layers/map_view_layer.dart
+++ lib/feactures/map/presentation/layers/map_view_layer.dart
@@ -9,10 +9,12 @@ class MapViewLayer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
     return FlutterMap(
       mapController: MapController(),
       options: const MapOptions(
-        initialZoom: 10,
+        initialZoom: 13,
         initialCenter: LatLng(-28.460501, -65.780756),
 
         // initialCameraFit: CameraFit.coordinates(coordinates: )
@@ -22,6 +24,37 @@ class MapViewLayer extends StatelessWidget {
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
           userAgentPackageName: 'dev.fleaflet.flutter_map.example',
           // Plenty of other options available!
+        ),
+        const CircleLayer(
+          circles: [
+            CircleMarker(
+                point: LatLng(-28.476765, -65.787757),
+                radius: 30,
+                useRadiusInMeter: true,
+                color: Color.fromARGB(139, 33, 149, 243)),
+          ],
+        ),
+        MarkerLayer(
+          markers: [
+            Marker(
+              point: const LatLng(-28.476765, -65.787757),
+              width: 50,
+              height: 50,
+              child: Icon(
+                Icons.location_on,
+                color: colors.primary,
+              ),
+            ),
+            Marker(
+              point: const LatLng(-28.460397, -65.767009),
+              width: 50,
+              height: 50,
+              child: Icon(
+                Icons.location_on,
+                color: colors.primary,
+              ),
+            ),
+          ],
         ),
       ],
     );
