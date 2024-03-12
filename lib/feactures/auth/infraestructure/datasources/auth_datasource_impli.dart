4e7beb32461218bfe6ab06118c905a1631919346
Maxi Alzugaray <maxialz99@hotmail.com>
2024-03-12 12:50:23 -0300

    creo funcionalidad en peticion http en datasourceImpl

--- lib/feactures/auth/infraestructure/datasources/auth_datasource_impli.dart
+++ lib/feactures/auth/infraestructure/datasources/auth_datasource_impli.dart
@@ -1,6 +1,11 @@
+import 'package:app_ciudadano_vc/config/constants/enviroments.dart';
 import 'package:app_ciudadano_vc/feactures/auth/domain/auth_domain.dart';
+import 'package:app_ciudadano_vc/feactures/auth/infraestructure/auth_infraestructure.dart';
+import 'package:dio/dio.dart';
 
 class AuthDatasourceImpl extends AuthDatasource {
+  final dio = Dio(BaseOptions(baseUrl: Enviroments.apiUrl));
+
   @override
   Future<User> checkAuthStatus(String token) {
     // TODO: implement checkAuthStatus
@@ -14,9 +19,19 @@ class AuthDatasourceImpl extends AuthDatasource {
   }
 
   @override
-  Future<User> sendPhoneNumber(int phoneNumber) {
-    // TODO: implement sendPhoneNumber
-    throw UnimplementedError();
+  Future<User> sendPhoneNumber(String phoneNumber) async {
+    try {
+      final response = await dio.post('/auth/local',
+          data: {'identifier': phoneNumber, 'password': phoneNumber});
+
+      final user = UserMapper.userJsonToEntity(response.data);
+
+      return user;
+    } catch (e) {
+      throw WrongCredentials();
+    }
+
+    // throw UnimplementedError();
   }
 
   @override
@@ -24,5 +39,4 @@ class AuthDatasourceImpl extends AuthDatasource {
     // TODO: implement sendVerificationCode
     throw UnimplementedError();
   }
-
 }
--- lib/feactures/auth/infraestructure/repositories/auth_repositori_impl.dart
+++ lib/feactures/auth/infraestructure/repositories/auth_repositori_impl.dart
@@ -1,27 +1,30 @@
-import 'package:app_ciudadano_vc/feactures/auth/domain/entities/user.dart';
+import 'package:app_ciudadano_vc/feactures/auth/domain/auth_domain.dart';
 import 'package:app_ciudadano_vc/feactures/auth/domain/repositories/auth_repository.dart';
+import 'package:app_ciudadano_vc/feactures/auth/infraestructure/auth_infraestructure.dart';
 
 class AuthRepositoryImpl extends AuthRepository {
+  final AuthDatasource dataSource;
+
+  AuthRepositoryImpl({AuthDatasource? datasource})
+      : dataSource = datasource ?? AuthDatasourceImpl();
+
   @override
   Future<User> checkAuthStatus(String token) {
-    // TODO: implement checkAuthStatus
-    throw UnimplementedError();
+    return dataSource.checkAuthStatus(token);
+  }
+
+  // @override
+  // Future<User> register( userRegistrationData) {
+  //   return dataSource.register( );
+  // }
+
+  @override
+  Future<User> sendPhoneNumber(String phoneNumber) {
+    return dataSource.sendPhoneNumber(phoneNumber);
   }
 
   @override
-  Future<User> register(UserRegistrationData userRegistrationData) {
-    // TODO: implement register
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<User> sendPhoneNumber(int phoneNumber) {
-    // TODO: implement sendPhoneNumber
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<User> sendVerificationCode(int phoneNumber) {
+  Future<User> sendVerificationCode(String phoneNumber) {
     // TODO: implement sendVerificationCode
     throw UnimplementedError();
   }
