cefe046f0f0e1cf158747aceea1d2b04afa0b936
Maxi Alzugaray <maxialz99@hotmail.com>
2024-03-19 12:48:11 -0300

    agrego notas en router

--- lib/config/router/app_router_notifier.dart
+++ lib/config/router/app_router_notifier.dart
@@ -2,8 +2,10 @@ import 'package:app_ciudadano_vc/feactures/auth/presentation/providers/auth_prov
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+/*
+  Esta clase es para poder asignarle al Router un refreshListenable
+ */
 final goRouterNotifierProvider = Provider((ref) {
-  
   final authNotifier = ref.read(authProvider.notifier);
 
   return GoRouterNofier(authNotifier);
@@ -11,10 +13,13 @@ final goRouterNotifierProvider = Provider((ref) {
 
 // Permite poner un lstener en el router para
 class GoRouterNofier extends ChangeNotifier {
+  // instancia del AuthNotifier para checquear el estado
   final AuthNotifier _authNotifier;
 
+// inicializacion del estado principal
   AuthStatus _authStatus = AuthStatus.checking;
 
+// Constructor que esta pendiente de los estados, por eso el addListener
   GoRouterNofier(this._authNotifier) {
     _authNotifier.addListener((state) {
       authStatus = state.authStatus!;
@@ -23,6 +28,7 @@ class GoRouterNofier extends ChangeNotifier {
 
   AuthStatus get authStatus => _authStatus;
 
+// Cuando cambie el estado de authStatus, autometicamente le notifica al router
   set authStatus(AuthStatus value) {
     _authStatus = value;
     notifyListeners();
