f0109b98eb3ae876c093e4d90fed8b413a404009
Maxi Alzugaray <maxialz99@hotmail.com>
2024-04-08 13:10:34 -0300

    creo provider nuevo para authform

--- lib/feactures/auth/presentation/providers/auth_form_provider.dart
+++ lib/feactures/auth/presentation/providers/auth_form_provider.dart
@@ -1,106 +1,58 @@
-// import 'package:app_ciudadano_vc/config/config.dart';
-import 'package:app_ciudadano_vc/feactures/auth/presentation/providers/auth_provider.dart';
-import 'package:app_ciudadano_vc/shared/infraestructure/inputs/inputs.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:formz/formz.dart';
-// import 'package:go_router/go_router.dart';
 
-// #3 crear instancia del proveedor para usarlo en el widget
 final authFormProvider =
     StateNotifierProvider<AuthFormNotifier, AuthFormState>((ref) {
-      
-  final loginUSerCallback = ref.watch(authProvider.notifier).loginUSer;
-
-  return AuthFormNotifier(loginUserCallback: loginUSerCallback);
+  return AuthFormNotifier();
 });
 
-// #1 Definir el estado del formulario:
 class AuthFormState {
-  final bool isLoading;
-
-  final bool isPhoneNumberSubmitted;
-  final bool isVerificationCodeSubmitted;
-
-  final bool isPhoneNumberValid;
-  final bool isVerificationCodeValid;
-
-  final String? errorMessage;
-  final PhoneNumber phoneNumber;
-  final ValidateCode verificationCode;
+  final String phoneNumber;
+  final String verificationCode;
+  final String phoneNumberUnmasked;
+  final String verificationCodeUnmasked;
 
   AuthFormState({
-    this.isLoading = false,
-    this.isPhoneNumberSubmitted = false,
-    this.isVerificationCodeSubmitted = false,
-    this.isPhoneNumberValid = false,
-    this.isVerificationCodeValid = false,
-    this.errorMessage,
-    // PhoneNumber? phoneNumber = const PhoneNumber.pure(),
-    this.phoneNumber = const PhoneNumber.pure(),
-    this.verificationCode = const ValidateCode.pure(),
-  }) /* : phoneNumber = phoneNumber ?? PhoneNumber.withDefaultCountryCode(); */;
+    this.phoneNumber = '',
+    this.verificationCode = '',
+    this.phoneNumberUnmasked = '',
+    this.verificationCodeUnmasked = '',
+  });
 
-  AuthFormState copyWith({
-    bool? isLoading,
-    bool? isPhoneNumberSubmitted,
-    bool? isVerificationCodeSubmitted,
-    bool? isPhoneNumberValid,
-    bool? isVerificationCodeValid,
-    String? errorMessage,
-    PhoneNumber? phoneNumber,
-    ValidateCode? verificationCode,
-  }) =>
-      AuthFormState(
-        isLoading: isLoading ?? this.isLoading,
-        isPhoneNumberSubmitted:
-            isPhoneNumberSubmitted ?? this.isPhoneNumberSubmitted,
-        isVerificationCodeSubmitted:
-            isVerificationCodeSubmitted ?? this.isVerificationCodeSubmitted,
-        isPhoneNumberValid: isPhoneNumberValid ?? this.isPhoneNumberValid,
-        isVerificationCodeValid:
-            isVerificationCodeValid ?? this.isVerificationCodeValid,
-        errorMessage: errorMessage ?? this.errorMessage,
-        phoneNumber: phoneNumber ?? this.phoneNumber,
-        verificationCode: verificationCode ?? this.verificationCode,
-      );
+  AuthFormState copyWith(
+      {String? phoneNumber,
+      String? verificationCode,
+      String? phoneNumberUnmasked,
+      String? verificationCodeUnmasked}) {
+    return AuthFormState(
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      verificationCode: verificationCode ?? this.verificationCode,
+      phoneNumberUnmasked: phoneNumberUnmasked ?? this.phoneNumberUnmasked,
+      verificationCodeUnmasked:
+          verificationCodeUnmasked ?? this.verificationCodeUnmasked,
+    );
+  }
 }
 
-// #2 notificador del estado del formualrio
 class AuthFormNotifier extends StateNotifier<AuthFormState> {
-  // enlazo el provider de riverpod con el login form
-  final Function(String) loginUserCallback;
+  AuthFormNotifier() : super(AuthFormState());
 
-  AuthFormNotifier({required this.loginUserCallback}) : super(AuthFormState());
-
-  onPhoneNumberChange(String value) {
-    final newPhoneNumber = PhoneNumber.dirty(value);
-
-    state = state.copyWith(
-        phoneNumber: newPhoneNumber,
-        isPhoneNumberValid:
-            Formz.validate([newPhoneNumber, state.phoneNumber]));
+  void printState() {
+    print(state.toString());
   }
 
-  onSubmitPhoneNumber(BuildContext context) async {
-    _touchedPhoneNumberField();
-    if (!state.isPhoneNumberValid) return;
-
-    final phoneNumberWithoutMask =
-        state.phoneNumber.value.replaceAll(RegExp(r'[^0-9]'), '');
-
-    await loginUserCallback(phoneNumberWithoutMask);
+  void setPhoneNumber({required String newPhoneNumber}) {
+    state = state.copyWith(phoneNumber: newPhoneNumber);
   }
 
-  _touchedPhoneNumberField() {
-    final phoneNumber = PhoneNumber.dirty(state.phoneNumber.value);
-    print('====maskFormatter====>>>>>>>>>>> $phoneNumber), ' ')}');
+  void setVerificationCode({required String newVerificationCode}) {
+    state = state.copyWith(verificationCode: newVerificationCode);
+  }
+
+  void setUnmaskedValues(
+      {required String phoneNumberUnmasked,
+      required String verificationCodeUnmasked}) {
     state = state.copyWith(
-        isLoading: false,
-        isPhoneNumberSubmitted: true,
-        phoneNumber: phoneNumber,
-        isPhoneNumberValid: Formz.validate(
-          [phoneNumber],
-        ));
+        verificationCodeUnmasked: verificationCodeUnmasked,
+        phoneNumberUnmasked: phoneNumberUnmasked);
   }
 }
