41fd9930149a1e40f32d9da46c72ffdb14170f1c
Maxi Alzugaray <maxialz99@hotmail.com>
2024-04-08 11:11:09 -0300

    register service implementado

--- lib/feactures/auth/presentation/widgets/register_upload_files.dart
+++ lib/feactures/auth/presentation/widgets/register_upload_files.dart
@@ -5,9 +5,8 @@ import 'package:app_ciudadano_vc/feactures/auth/service/register_service.dart';
 import 'package:app_ciudadano_vc/shared/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
-final isLoadingProvider = StateProvider<bool>((ref) {
+final isLoadingProvider = StateProvider.autoDispose<bool>((ref) {
   return false;
 });
 
@@ -20,31 +19,11 @@ class UploadFiles extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final titleStyle = Theme.of(context).textTheme.titleLarge;
     final subtitleStyle = Theme.of(context).textTheme.titleMedium;
-    final isLoading = ref.watch(isLoadingProvider);
 
-    return isLoading
-        ? const FullLoader()
-        : Scaffold(
-            body: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Image.asset('assets/images/vamos-en-bici-01.png', width: 250),
-                  _UploadFilesView(
-                      titleStyle: titleStyle, subtitleStyle: subtitleStyle)
-                  // _NavigateButtons(
-                  //     currentPageIndex: currentPageIndex,
-                  //     pageController: pageController),
-                ],
-              ),
-            ),
-          );
+    return Scaffold(
+      body: _UploadFilesView(
+          titleStyle: titleStyle, subtitleStyle: subtitleStyle),
+    );
   }
 }
 
@@ -68,6 +47,7 @@ class _UploadFilesView extends ConsumerWidget {
     final registerForm = ref.watch(registerFormProvider);
     final fileRegisterForm = ref.watch(filesRegisterFormProvider);
     final routerProvider = ref.watch(goRouterProvider);
+    final isLoading = ref.watch(isLoadingProvider);
 
     final RegisterServices registerService = RegisterServices();
 
@@ -77,121 +57,147 @@ class _UploadFilesView extends ConsumerWidget {
     const textStylesButton =
         TextStyle(fontWeight: FontWeight.normal, fontSize: 20);
 
-    void onPressRegisterUser() async {
-      // ref.watch(isLoadingProvider.notifier).update((state) => true);
+    void showRegistrationSuccessSnackbar(
+        {required BuildContext context,
+        required String label,
+        required Color color}) {
+      final snackBar = SnackBar(
+        content: Text(label),
+        backgroundColor: color,
+        action: SnackBarAction(
+          label: 'Cerrar',
+          onPressed: () {},
+        ),
+      );
 
-      // try {
-      //   final serviceResponse = await registerService.registerUser(
-      //       files: fileRegisterForm, userData: registerForm);
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
 
-      //   if (serviceResponse.statusCode == 201) {
-      //     // // ref.read(goRouterProvider).push('/home');
-      //     // context.push(location)
-      //     //  TODO: MAndarlo al login aqui
-      //   }
-      // } catch (e) {
-      //   // TODO: Mostrar error en pantalla
-      //   // ref.read(goRouterProvider).push('/register');
-      //   // context.push('/register');
-      //   Exception(e);
-      //   // ref.watch(isLoadingProvider.notifier).update((state) => false);
-      // }
-      // ref.watch(isLoadingProvider.notifier).update((state) => false);
+    void onPressRegisterUser(context) async {
+      ref.watch(isLoadingProvider.notifier).update((state) => true);
 
+      try {
+        final serviceResponse = await registerService.registerUser(
+            files: fileRegisterForm, userData: registerForm);
+
+        if (serviceResponse.statusCode == 201) {
+          showRegistrationSuccessSnackbar(
+              context: context,
+              color: Colors.green,
+              label: '¡Registro exitoso!');
+          routerProvider.push('/auth');
+          // context.push(location)
+        }
+      } catch (e) {
+        showRegistrationSuccessSnackbar(
+            context: context, color: Colors.red, label: 'Error al registrarse');
+        Exception(e);
+      }
       ref.read(goRouterProvider).push('/auth');
     }
 
-    return Container(
-      // color: Colors.black,
-      alignment: Alignment.center,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            children: [
-              Text(
-                '¡Ultimo paso ${registerForm.name}!',
-                textAlign: TextAlign.center,
-                style: titleStyle,
+    return isLoading
+        ? const FullLoader()
+        : SingleChildScrollView(
+            child: Container(
+              // color: Colors.black,
+              alignment: Alignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Image.asset('assets/images/vamos-en-bici-01.png', width: 250),
+                  Column(
+                    children: [
+                      Text(
+                        '¡Ultimo paso ${registerForm.name}!',
+                        textAlign: TextAlign.center,
+                        style: titleStyle,
+                      ),
+                      Text(
+                        'Necesitaremos  algunas fotos para continuar',
+                        style: subtitleStyle,
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      CustomOutlineButtom(
+                        text: 'Foto de tu rostro',
+                        icon: fileRegisterForm.photoUserPath.isNotEmpty
+                            ? const Icon(Icons.check)
+                            : null,
+                        // icon: const Icon(Icons.abc),
+                        statesController: photoFacePathController,
+                        onPressed: () async {
+                          final photoPaht =
+                              await CameraGalleryServiceImpl().takePhoto();
+                          if (photoPaht == null) return;
+                          return ref
+                              .read(filesRegisterFormProvider.notifier)
+                              .setPhotoUserDevicePath(value: photoPaht);
+                        },
+                      ),
+                      _SpacerInfo(
+                          titleStyle: titleStyle,
+                          subtitleStyle: subtitleStyle,
+                          registerForm: registerForm),
+                      CustomOutlineButtom(
+                        statesController: photonDinFrontPath,
+                        text: 'Foto frente DNI',
+                        icon: fileRegisterForm.photoDniFrontPath.isNotEmpty
+                            ? const Icon(Icons.check)
+                            : null,
+                        onPressed: () async {
+                          final photoPaht =
+                              await CameraGalleryServiceImpl().takePhoto();
+                          if (photoPaht == null) return;
+                          return ref
+                              .read(filesRegisterFormProvider.notifier)
+                              .setPhotoDniFrontDevicePath(value: photoPaht);
+                        },
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      CustomOutlineButtom(
+                        statesController: photonDinFrontPath,
+                        text: 'Foto dorzo DNI',
+                        icon: fileRegisterForm.photoDniBackPath.isNotEmpty
+                            ? const Icon(Icons.check)
+                            : null,
+                        onPressed: () async {
+                          final photoPaht =
+                              await CameraGalleryServiceImpl().takePhoto();
+                          if (photoPaht == null) return;
+                          ref
+                              .read(filesRegisterFormProvider.notifier)
+                              .setPhotoDniBackDevicePath(value: photoPaht);
+                        },
+                      ),
+                      const SizedBox(
+                        height: 60,
+                      ),
+                      FilledButton(
+                          style: buttonStyles,
+                          // onPressed: () => registerUSer(),
+                          onPressed: fileRegisterForm.isFilesPathsCompleted
+                              ? () => onPressRegisterUser(context)
+                              : null,
+                          child: const Text(
+                            'Finalizar registro',
+                            style: textStylesButton,
+                          ))
+                    ],
+                  ),
+                ],
               ),
-              Text(
-                'Necesitaremos  algunas fotos para continuar',
-                style: subtitleStyle,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              CustomOutlineButtom(
-                text: 'Foto de tu rostro',
-                icon: fileRegisterForm.photoUserPath.isNotEmpty
-                    ? const Icon(Icons.check)
-                    : null,
-                // icon: const Icon(Icons.abc),
-                statesController: photoFacePathController,
-                onPressed: () async {
-                  final photoPaht =
-                      await CameraGalleryServiceImpl().takePhoto();
-                  if (photoPaht == null) return;
-                  return ref
-                      .read(filesRegisterFormProvider.notifier)
-                      .setPhotoUserDevicePath(value: photoPaht);
-                },
-              ),
-              _SpacerInfo(
-                  titleStyle: titleStyle,
-                  subtitleStyle: subtitleStyle,
-                  registerForm: registerForm),
-              CustomOutlineButtom(
-                statesController: photonDinFrontPath,
-                text: 'Foto frente DNI',
-                icon: fileRegisterForm.photoDniFrontPath.isNotEmpty
-                    ? const Icon(Icons.check)
-                    : null,
-                onPressed: () async {
-                  final photoPaht =
-                      await CameraGalleryServiceImpl().takePhoto();
-                  if (photoPaht == null) return;
-                  return ref
-                      .read(filesRegisterFormProvider.notifier)
-                      .setPhotoDniFrontDevicePath(value: photoPaht);
-                },
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              CustomOutlineButtom(
-                statesController: photonDinFrontPath,
-                text: 'Foto dorzo DNI',
-                icon: fileRegisterForm.photoDniBackPath.isNotEmpty
-                    ? const Icon(Icons.check)
-                    : null,
-                onPressed: () async {
-                  final photoPaht =
-                      await CameraGalleryServiceImpl().takePhoto();
-                  if (photoPaht == null) return;
-                  ref
-                      .read(filesRegisterFormProvider.notifier)
-                      .setPhotoDniBackDevicePath(value: photoPaht);
-                },
-              ),
-              const SizedBox(
-                height: 60,
-              ),
-              FilledButton(
-                  style: buttonStyles,
-                  // onPressed: () => registerUSer(),
-                  onPressed: fileRegisterForm.isFilesPathsCompleted
-                      ? () => onPressRegisterUser()
-                      : null,
-                  child: const Text(
-                    'Finalizar registro',
-                    style: textStylesButton,
-                  ))
-            ],
-          ),
-        ],
-      ),
-    );
+            ),
+          );
   }
 }
 
