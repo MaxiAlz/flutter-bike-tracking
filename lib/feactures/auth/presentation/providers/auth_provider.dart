f7415a45ca4248822cf971dc6ac9870b312adef7
Maxi Alzugaray <maxialz99@hotmail.com>
2024-04-10 13:09:39 -0300

    creo provider para authenticacion

---
+++ lib/feactures/auth/presentation/providers/auth_provider.dart
@@ -0,0 +1,55 @@
+import 'package:app_ciudadano_vc/feactures/auth/domain/entities/auth_status.dart';
+import 'package:app_ciudadano_vc/feactures/auth/domain/entities/user.dart';
+import 'package:app_ciudadano_vc/feactures/auth/service/auth_service.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
+  final authServices = AuthServices();
+  return AuthNotifier(authServices);
+});
+
+class AuthState {
+  final User? user;
+  final String errorMessage;
+  final AuthStatus authStatus;
+
+  AuthState(
+      {this.user,
+      this.errorMessage = '',
+      this.authStatus = AuthStatus.checking});
+
+  AuthState copyWith({
+    User? user,
+    String? errorMessage,
+    AuthStatus? authStatus,
+  }) {
+    return AuthState(
+      user: user ?? this.user,
+      errorMessage: errorMessage ?? this.errorMessage,
+      authStatus: authStatus ?? this.authStatus,
+    );
+  }
+}
+
+class AuthNotifier extends StateNotifier<AuthState> {
+  final AuthServices authService;
+  AuthNotifier(this.authService) : super(AuthState());
+
+  Future sendPhoneToVerification({required String phoneNumber}) async {
+    state = state.copyWith(authStatus: AuthStatus.checking);
+    try {
+      final serviceResponse = await authService.sendPhoneService(phoneNumber);
+
+      if (serviceResponse.response.data['statuscode'] == 404) {
+        state = state.copyWith(authStatus: AuthStatus.notRegistered);
+        // TODO: Aqui hacer que lo lleve a la pestaña de registro si no se encuentra el usuario
+        return;
+      }
+
+      return serviceResponse;
+    } on DioException catch (error) {
+      return error;
+    }
+  }
+}
