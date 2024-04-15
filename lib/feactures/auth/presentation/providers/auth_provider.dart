87b34e6517edb678e32512bfe9b4ce7a6174047c
Maxi Alzugaray <maxialz99@hotmail.com>
2024-04-15 13:25:17 -0300

    implemento metodo para checkear estado de la autenticacion del usuario

--- lib/feactures/auth/presentation/providers/auth_provider.dart
+++ lib/feactures/auth/presentation/providers/auth_provider.dart
@@ -1,12 +1,17 @@
 import 'package:app_ciudadano_vc/feactures/auth/domain/entities/auth_status.dart';
 import 'package:app_ciudadano_vc/feactures/auth/domain/entities/user.dart';
+import 'package:app_ciudadano_vc/feactures/auth/infraestructure/mappers/user_mapper.dart';
 import 'package:app_ciudadano_vc/feactures/auth/service/auth_service.dart';
+import 'package:app_ciudadano_vc/shared/infraestructure/services/shared_preferences/key_value_storage_impl.dart';
+import 'package:app_ciudadano_vc/shared/infraestructure/services/shared_preferences/key_value_storage_service.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
   final authServices = AuthServices();
-  return AuthNotifier(authServices);
+  final setKeyValue = KeyValueStorageImpl();
+
+  return AuthNotifier(authService: authServices, keyValueStorage: setKeyValue);
 });
 
 class AuthState {
@@ -34,7 +39,12 @@ class AuthState {
 
 class AuthNotifier extends StateNotifier<AuthState> {
   final AuthServices authService;
-  AuthNotifier(this.authService) : super(AuthState());
+  final KeyValueStorageService keyValueStorage;
+
+  AuthNotifier({required this.authService, required this.keyValueStorage})
+      : super(AuthState()) {
+    checkAuthStatus();
+  }
 
   Future sendPhoneToVerification({required String phoneNumber}) async {
     state = state.copyWith(authStatus: AuthStatus.checking);
@@ -43,6 +53,7 @@ class AuthNotifier extends StateNotifier<AuthState> {
 
       if (serviceResponse.statusCode == 201) {
         state = state.copyWith(authStatus: AuthStatus.registered);
+
         return serviceResponse;
       }
 
@@ -54,6 +65,59 @@ class AuthNotifier extends StateNotifier<AuthState> {
       return serviceResponse;
     } on DioException catch (error) {
       return error;
+    }
+  }
+
+  Future loginUser({
+    required String phoneNumber,
+    required String code,
+  }) async {
+    state = state.copyWith(authStatus: AuthStatus.checking);
+    try {
+      final serviceResponse = await authService.verifyCodeService(
+          phoneNumber: phoneNumber, code: code);
+
+      if (serviceResponse.statusCode == 201) {
+        final token = serviceResponse.headers['authorization'][0];
+        final userJson = serviceResponse.data as Map<String, dynamic>;
+        final User user = UserMapper.userJsonToEntity(userJson);
+        await keyValueStorage.setStringKeyValue('userToken', token);
+        state =
+            state.copyWith(authStatus: AuthStatus.authenticated, user: user);
+        return serviceResponse;
+      }
+
+      if (serviceResponse.statusCode == 404) {
+        state = state.copyWith(authStatus: AuthStatus.notAuthenticated);
+        return serviceResponse;
+      }
+
+      return serviceResponse;
+    } on DioException catch (error) {
+      return error;
+    }
+  }
+
+  Future<void> logoutUSer() async {
+    await keyValueStorage.removeKey('userToken');
+    state = state.copyWith(authStatus: AuthStatus.notAuthenticated, user: null);
+  }
+
+  void checkAuthStatus() async {
+    final userToken = await keyValueStorage.getKeyValue<String>('userToken');
+    if (userToken == null) return logoutUSer();
+
+    try {
+      final serviceResponse =
+          await authService.checkAuthStatusService(userToken);
+      final user = UserMapper.userJsonToEntity(serviceResponse.data);
+
+      final newToken =
+          serviceResponse.headers['authorization'][0].replaceAll('Bearer ', '');
+      await keyValueStorage.setStringKeyValue('userToken', newToken);
+      state = state.copyWith(authStatus: AuthStatus.authenticated, user: user);
+    } catch (e) {
+      logoutUSer();
     }
   }
 }
