f086954626c949dff289aa167078ac375a40fa00
Maxi Alzugaray <maxialz99@hotmail.com>
2024-04-16 11:37:17 -0300

    creo clases abstractas para servicios de registro y login

--- lib/feactures/auth/domain/auth_domain.dart
+++ lib/feactures/auth/domain/auth_domain.dart
@@ -1,4 +1,4 @@
-
+export 'package:app_ciudadano_vc/feactures/auth/domain/services/auth_services_domain.dart';
 
 export 'package:app_ciudadano_vc/feactures/auth/domain/datasources/auth_datasource.dart';
 export 'package:app_ciudadano_vc/feactures/auth/domain/entities/user_registration_data.dart';
