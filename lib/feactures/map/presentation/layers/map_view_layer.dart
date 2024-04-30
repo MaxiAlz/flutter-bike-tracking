6f2d8fd9c68e0a730c254bab51e14614333aaf75
Maxi Alzugaray <maxialz99@hotmail.com>
2024-04-30 11:08:45 -0300

    pinto estaciones en la vista del mapa

--- lib/feactures/map/presentation/layers/map_view_layer.dart
+++ lib/feactures/map/presentation/layers/map_view_layer.dart
@@ -1,8 +1,8 @@
 // import 'package:app_ciudadano_vc/shared/infraestructure/services/geolocation/geolocation_service_impl.dart';
 import 'package:app_ciudadano_vc/feactures/home/presentation/home_presentation.dart';
 import 'package:app_ciudadano_vc/feactures/map/presentation/layers/marker_dialog_hub.dart';
+import 'package:app_ciudadano_vc/feactures/map/presentation/providers/hubs_provider.dart';
 import 'package:app_ciudadano_vc/feactures/map/presentation/providers/map_settings_provider.dart';
-import 'package:app_ciudadano_vc/shared/infraestructure/services/geolocation/markers_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -19,66 +19,81 @@ class MapViewLayer extends ConsumerWidget {
     final mapSettings = ref.watch(mapSettingProvider);
     final markerDialogHub = MarkerDialogHub();
     final mapController = MapController();
+    final hubList = ref.watch(hubListProvider);
+
+    print('===>::hubList::==> $hubList');
 
     // getUserPosition() {
     //   final position = ref.read(mapSettingProvider.notifier).getUserPosition();
     //   print('&&&position&&& : $position');
     // }
 
-    return FlutterMap(
-      mapController: mapController,
-      options: const MapOptions(
-        initialZoom: 13,
-        initialCenter: LatLng(-28.460501, -65.780756),
+    return hubList.when(
+      data: (listOfHubs) => FlutterMap(
+        mapController: mapController,
+        options: const MapOptions(
+          initialZoom: 13,
+          initialCenter: LatLng(-28.460501, -65.780756),
+        ),
+        children: [
+          TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+            // Plenty of other options available!
+          ),
+          // const CircleLayer(
+          //   circles: [
+          //     CircleMarker(
+          //         point: LatLng(-28.476765, -65.787757),
+          //         radius: 30,
+          //         useRadiusInMeter: true,
+          //         color: Color.fromARGB(139, 33, 149, 243)),
+          //   ],
+          // ),
+          Container(
+            alignment: Alignment.topRight,
+            child: Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 35),
+                child: MyLocationButton(
+                  mapController: mapController,
+                  mapSetting: mapSettings,
+                )),
+          ),
+          // MarkerLayer(markers: [
+          //   Marker(
+          //       point: LatLng(mapSettings.userPosition.latitude,
+          //           mapSettings.userPosition.longitude),
+          //       child: const Icon(
+          //         Icons.location_on,
+          //         color: Colors.red,
+          //       ))
+          // ]),
+
+          MarkerLayer(
+            markers: hubList.value!
+                .map(
+                  (marker) => Marker(
+                      point:
+                          LatLng(marker.ubicacion.lat, marker.ubicacion.long),
+                      width: 50.0, // Use double for width and height
+                      height: 50.0,
+                      child: IconButton.filled(
+                          onPressed: () =>
+                              markerDialogHub.onMarkerTap(context, marker),
+                          icon: const Icon(Icons.store_mall_directory))),
+                )
+                .toList(),
+          ),
+        ],
       ),
-      children: [
-        TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'dev.fleaflet.flutter_map.example',
-          // Plenty of other options available!
-        ),
-        const CircleLayer(
-          circles: [
-            CircleMarker(
-                point: LatLng(-28.476765, -65.787757),
-                radius: 30,
-                useRadiusInMeter: true,
-                color: Color.fromARGB(139, 33, 149, 243)),
-          ],
-        ),
-        Container(
-          alignment: Alignment.topRight,
-          child: Padding(
-              padding: const EdgeInsets.fromLTRB(0, 0, 0, 35),
-              child: MyLocationButton(
-                mapController: mapController,
-                mapSetting: mapSettings,
-              )),
-        ),
-        MarkerLayer(markers: [
-          Marker(
-              point: LatLng(mapSettings.userPosition.latitude,
-                  mapSettings.userPosition.longitude),
-              child: const Icon(
-                Icons.location_on,
-                color: Colors.red,
-              ))
-        ]),
-        MarkerLayer(
-          markers: marcadoresData
-              .map(
-                (marker) => Marker(
-                    point: marker.position,
-                    width: 50.0, // Use double for width and height
-                    height: 50.0,
-                    child: IconButton.filled(
-                        onPressed: () =>
-                            markerDialogHub.onMarkerTap(context, marker),
-                        icon: const Icon(Icons.store_mall_directory))),
-              )
-              .toList(),
-        ),
-      ],
+      loading: () {
+        // Mientras se está cargando la lista de hubs, puedes mostrar un indicador de carga
+        return const CircularProgressIndicator();
+      },
+      error: (error, stackTrace) {
+        // Si ocurre un error durante la carga de la lista de hubs, puedes manejarlo aquí
+        return Text('Error al cargar la lista de hubs: $error');
+      },
     );
   }
 }
--- lib/feactures/map/presentation/layers/marker_dialog_hub.dart
+++ lib/feactures/map/presentation/layers/marker_dialog_hub.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 // tambien podria ser una carpeta de util osea => MarkersUtil y crear todos los metodos aca
 class MarkerDialogHub {
   void onMarkerTap(BuildContext context, marker) {
@@ -10,6 +9,7 @@ class MarkerDialogHub {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
+          elevation: 20,
           title: Column(
             children: [
               Row(
@@ -22,28 +22,57 @@ class MarkerDialogHub {
                   Column(
                     children: [
                       Text(
-                        '${marker.title}',
+                        '${marker.nombre}',
                         style: subTitleStyle.titleMedium,
-                      ),
-                      Text(
-                        '${marker.description}',
-                        style: subTitleStyle.bodySmall,
                       ),
                     ],
                   ),
                 ],
+              ),
+              Text(
+                '${marker.direccion}',
+                style: subTitleStyle.bodySmall,
               ),
               const SizedBox(
                 child: Divider(),
               )
             ],
           ),
-          content: Row(
-            children: [
-              const Icon(Icons.pedal_bike_rounded),
-              Padding(padding: const EdgeInsets.symmetric(horizontal: 10)),
-              Text('Bicicletas disponibles: ${marker.availableBikes}'),
-            ],
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const Icon(Icons.directions_bike),
+                    const Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
+                    Text(
+                        'Bicicletas disponibles: ${marker.cantidadBicicletas}'),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    const Icon(Icons.electric_bike_outlined),
+                    const Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
+                    Text('Electricas: ${marker.capacidadElectrica}'),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    const Icon(Icons.pedal_bike_outlined),
+                    const Padding(padding: EdgeInsets.symmetric(horizontal: 5)),
+                    Text('Mecanicas: ${marker.capacidadMecanica}'),
+                  ],
+                ),
+              ],
+            ),
           ),
           actions: [
             TextButton(
