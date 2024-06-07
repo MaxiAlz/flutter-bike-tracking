d3b814874e6f08fcdee5b4369ba8be1e555bc630
Maxi Alzugaray <maxialz99@hotmail.com>
2024-06-07 12:38:11 -0300

    s

--- lib/feactures/trips/trips.dart
+++ lib/feactures/trips/trips.dart
@@ -1,3 +1,4 @@
 // #### Screens
+export 'package:app_ciudadano_vc/feactures/trips/presentation/screens/enter_bike_patent_screen.dart';
 export 'package:app_ciudadano_vc/feactures/trips/presentation/screens/trip_in_progress_screen.dart';
 export 'package:app_ciudadano_vc/feactures/trips/presentation/screens/my_trips_screen.dart';
