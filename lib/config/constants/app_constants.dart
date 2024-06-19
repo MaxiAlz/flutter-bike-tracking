cd4eb686e08bcf310ffeb63ea6120e54d3d1b563
Maxi Alzugaray <maxialz99@hotmail.com>
2024-06-19 10:24:06 -0300

    creo contstantes para llaves de storage

---
+++ lib/config/constants/app_constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  final String tripStatusKey = 'TRIP_STATUS';
+  final String timeStartTripkey = 'TIME_START_TRIP';
+  final String isTripInProgress = 'IS_TRIP_IN_PROGRES';
+  final String tokenKey = 'userToken';
+}
