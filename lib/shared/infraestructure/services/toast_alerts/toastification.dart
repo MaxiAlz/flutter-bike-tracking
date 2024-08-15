c75aa85de3560e57b157f035e2e0ee0e11970f7f
Maxi Alzugaray <maxialz99@hotmail.com>
2024-08-15 11:19:10 -0300

    modifico widgets de notificacion del usuario

--- lib/shared/infraestructure/services/toast_alerts/toastification.dart
+++ lib/shared/infraestructure/services/toast_alerts/toastification.dart
@@ -4,15 +4,47 @@ import 'package:toastification/toastification.dart';
 class ToastificationService {
   final toastification = Toastification();
 
-  void showToast(
-      {String? message,
-      String? title,
-      required ToastificationType typeOfToast}) {
+  void showSuccesToast({
+    String? message,
+    String? title,
+  }) {
     toastification.show(
-      type: typeOfToast,
+      type: ToastificationType.success,
       style: ToastificationStyle.flatColored,
       title: Text(title ?? "Exito"),
       description: Text(message ?? "Accion exitosa"),
+      alignment: Alignment.topCenter,
+      autoCloseDuration: const Duration(seconds: 4),
+      boxShadow: lowModeShadow,
+      dragToClose: true,
+    );
+  }
+
+  void showErrorToast({
+    String? message,
+    String? title,
+  }) {
+    toastification.show(
+      type: ToastificationType.error,
+      style: ToastificationStyle.flatColored,
+      title: Text(title ?? "Error"),
+      description: Text(message ?? "Ah ocurrido un error,Pruebe mas tarde"),
+      alignment: Alignment.topCenter,
+      autoCloseDuration: const Duration(seconds: 4),
+      boxShadow: lowModeShadow,
+      dragToClose: true,
+    );
+  }
+
+  void showInfoToast({
+    String? message,
+    String? title,
+  }) {
+    toastification.show(
+      type: ToastificationType.info,
+      style: ToastificationStyle.flatColored,
+      title: Text(title ?? "Info"),
+      description: Text(message ?? "Presta atencion a esta alerta"),
       alignment: Alignment.topCenter,
       autoCloseDuration: const Duration(seconds: 4),
       boxShadow: lowModeShadow,
