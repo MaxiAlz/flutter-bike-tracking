1bfcfea92cc6da87b9ed7c2a651d7c8eb704a721
Maxi Alzugaray <maxialz99@hotmail.com>
2024-08-20 10:12:13 -0300

    modifico pantalla de viajes para consumir provider

--- lib/feactures/trips/insfraestructure/service/trip_services.dart
+++ lib/feactures/trips/insfraestructure/service/trip_services.dart
@@ -1,7 +1,10 @@
 import 'package:app_ciudadano_vc/config/config.dart';
+import 'package:app_ciudadano_vc/feactures/trips/domain/entities/viaje_entity.dart';
+import 'package:app_ciudadano_vc/shared/infraestructure/share_infraestructure.dart';
 import 'package:dio/dio.dart';
 
 final Dio dio = Dio(BaseOptions(baseUrl: Enviroments.apiUrl));
+final api = Api();
 
 class TripServices {
   Future sendTravelRequestService(
@@ -14,6 +17,15 @@ class TripServices {
       return await dio.post('/viaje/solicitud', data: dataTrip);
     } on DioException catch (error) {
       return error.response;
+    }
+  }
+
+  Future<MyTripsPaginated> getMyTrips() async {
+    try {
+      final apiResponse = await api.getHttp(path: '/viaje');
+      return MyTripsPaginated.fromJson(apiResponse.data);
+    } catch (e) {
+      throw Exception('Failed to load trips: $e');
     }
   }
 }
--- lib/feactures/trips/presentation/screens/my_trips_screen.dart
+++ lib/feactures/trips/presentation/screens/my_trips_screen.dart
@@ -1,5 +1,7 @@
+import 'package:app_ciudadano_vc/feactures/trips/presentation/providers/my_trips_provider.dart';
 import 'package:app_ciudadano_vc/shared/widgets/headers/custom_top_header.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class MyTripsSCreen extends StatelessWidget {
   const MyTripsSCreen({super.key});
@@ -7,67 +9,89 @@ class MyTripsSCreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colors = Theme.of(context).colorScheme;
-    final subtitlesStyle = Theme.of(context).textTheme.titleMedium;
     return Scaffold(
       appBar: AppBar(
         iconTheme: const IconThemeData(
-          color: Colors.white, // Color blanco para el icono de retroceso
+          color: Colors.white, 
         ),
         centerTitle: true,
         backgroundColor: colors.primary,
-        title: Text(
-          'Mis viajes',
-          style: subtitlesStyle!.copyWith(fontSize: 18, color: Colors.white),
-        ),
       ),
       body: const _BodyTripsInfo(),
     );
   }
 }
 
-class _BodyTripsInfo extends StatelessWidget {
-  const _BodyTripsInfo({
-    super.key,
-  });
+class _BodyTripsInfo extends ConsumerWidget {
+  const _BodyTripsInfo();
 
   @override
-  Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: Center(
-        child: Column(children: [
-          CustomTopHeader(
-            titleHeader: 'Historial de viajes',
-            icon: Icons.pedal_bike_outlined,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-            child: Column(
-              children: [
-                ListTile(
-                  title: Text(
-                    'Viaje: N° 1',
-                  ),
-                  leading: const Icon(Icons.directions_bike_rounded),
-                  subtitle: const Text('Nodo tecnologico - Plaza 25 de mayo'),
-                  trailing: Text('20/04/2024'),
-                  isThreeLine: false,
+  Widget build(BuildContext context, WidgetRef ref) {
+    final myTripsData = ref.watch(myTripProvider);
+
+    return myTripsData.when(
+      data: (myTrips) => SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const CustomTopHeader(
+                titleHeader: 'Mis viajes realizados',
+                icon: Icons.pedal_bike_outlined,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                child: Column(
+                  children: myTrips.items.map((viaje) {
+                    return Column(
+                      children: [
+                        ListTile(
+                          title: Text('Viaje: N° ${viaje.id}'),
+                          leading: const Icon(Icons.directions_bike_rounded),
+                          subtitle: Text(
+                              '${viaje.estacionInicioId} - ${viaje.estacionFinalId ?? "En curso"}'),
+                          trailing: Text('Duracion: ${viaje.duracion}'),
+                          isThreeLine: false,
+                        ),
+                        const Divider(),
+                      ],
+                    );
+                  }).toList(),
                 ),
-                Divider(),
-                ListTile(
-                  title: Text(
-                    'Viaje: 20/04/2024',
-                  ),
-                  leading: const Icon(Icons.directions_bike_rounded),
-                  subtitle: const Text('Nodo tecnologico - Plaza 25 de mayo'),
-                  // trailing: Text('20/04/2024'),
-                  isThreeLine: false,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text(
+                        'Viaje: N° 1',
+                      ),
+                      leading: Icon(Icons.directions_bike_rounded),
+                      subtitle: Text('Nodo tecnologico - Plaza 25 de mayo'),
+                      trailing: Text('20/04/2024'),
+                      isThreeLine: false,
+                    ),
+                    Divider(),
+                    ListTile(
+                      title: Text(
+                        'Viaje: 20/04/2024',
+                      ),
+                      leading: Icon(Icons.directions_bike_rounded),
+                      subtitle: Text('Nodo tecnologico - Plaza 25 de mayo'),
+                      // trailing: Text('20/04/2024'),
+                      isThreeLine: false,
+                    ),
+                    Divider(),
+                  ],
                 ),
-                Divider(),
-              ],
-            ),
+              )
+            ],
           ),
-        ]),
+        ),
       ),
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (err, stack) => Center(child: Text('Error al cargar viajes, pruebe mas tarde')),
     );
   }
 }
